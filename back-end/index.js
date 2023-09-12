88286bce07e7365b74a3dc35d66c6ac1b78ee97e
Sam <ibtissamommari@gmail.com>
2023-09-13 00:45:53 +0200

    make server port configurable.
    since port 3000 can be used for other purposes.
    
    Remove the PORT constant since it isn't particularly useful now
    that we can pull the variable from the environment. It was above
    dotenv.config() anyways, so it couldn't have picked it up.

--- back-end/index.js
+++ back-end/index.js
@@ -5,17 +5,15 @@ const app = express();
 const dotenv = require('dotenv');
 const mysql = require('mysql2');
 
-const PORT = 3000; //El puerto 3000 es el puerto por defecto de Express
-
 dotenv.config(); //Configuración de dotenv
 
 app.get('/', (req, res) => {
   res.send('This server is now live!');
 }); //Ruta para mostrar un mensaje en la raíz del servidor
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-}); //El servidor escucha en el puerto 3000
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Server is running on port ${process.env.SERVER_PORT}`);
+});
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
