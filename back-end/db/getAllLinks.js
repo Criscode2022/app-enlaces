00cbbc8b2db4fb6f5e06e236a1988783a8b36931
Ignacio S.P <137695280+Nachosprada@users.noreply.github.com>
2023-09-13 16:22:50 +0200

    Update getAllLinks.js

--- back-end/db/getAllLinks.js
+++ back-end/db/getAllLinks.js
@@ -1,1 +1,16 @@
+const { getConnection } = require('./db');
 
+const getAllLinks = async () => {
+  let connection;
+  try {
+   connection = await getConnection();
+
+   const [result] = await connection.query(`
+      SELECT * FROM posts ORDER BY created_at DESC
+    `);
+
+    return result;
+  } finally {
+    if (connection) connection.release();
+  }
+};
