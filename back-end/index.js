3183cdc3b51c60ccc047c3169a1a0a9ceb86c796
Cristian Damil <cristiancode2023@gmail.com>
2023-09-13 09:08:07 +0200

    conexión a la base de datos usando pool y endpoint de prueba

--- back-end/index.js
+++ back-end/index.js
@@ -1,36 +1,50 @@
-//Importaciones usando CommonJS
-
+// Importaciones usando CommonJS
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
-const mysql = require('mysql2'); 
+const mysql = require('mysql2');
 
+const PORT = 3000; // El puerto 3000 es el puerto por defecto de Express
 
-const PORT = 3000; //El puerto 3000 es el puerto por defecto de Express
+dotenv.config(); // Configuración de dotenv
 
-dotenv.config(); //Configuración de dotenv
+// Creación del pool de conexiones a la base de datos
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+  database: 'enlaces',
+  connectionLimit: 100
+});
 
 app.get('/', (req, res) => {
-    res.send('This server is now live!');
-  }); //Ruta para mostrar un mensaje en la raíz del servidor
+  res.send('This server is now live!');
+}); // Ruta para mostrar un mensaje en la raíz del servidor
+
+app.get('/users', (req, res) => {
+  // Obtener una conexión del pool
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error('Error obteniendo conexión a la base de datos:', err);
+      return res.status(500).send('Error obteniendo conexión a la base de datos');
+    }
+
+    // Consulta a la base de datos
+    connection.query('SELECT * FROM users', (error, results, fields) => {
+      connection.release(); // Devolver la conexión al pool
+
+      if (error) {
+        console.error('Error ejecutando la consulta:', error);
+        return res.status(500).send('Error ejecutando la consulta');
+      }
+
+      console.log('Resultados de la consulta:', results);
+      res.json(results);
+    });
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-}); //El servidor escucha en el puerto 3000
-
-
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT
-  });
-  
-  connection.connect((err) => {
-    if (err) {
-      console.error('Error connecting to the database:', err);
-      return;
-    }
-    console.log('Connected to the database!');
-  });
-  
+}); // El servidor escucha en el puerto 3000
