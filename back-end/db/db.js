720799fcefb6ca26e12a4d56f970852268a5cba3
Ignacio S.P <137695280+Nachosprada@users.noreply.github.com>
2023-09-13 16:47:22 +0200

    Update db.js

--- back-end/db/db.js
+++ back-end/db/db.js
@@ -1,6 +1,8 @@
 const mysql = require('mysql2/promise');
+//Desestructuramos las variables de entorno:
 const { DB_PORT, DB_HOST, DB_USER, DB_PASS, DB_DATABASE } = process.env;
 
+//Creamos la función de conexión a la base de datos que exportaremos al final del código:
 let pool;
 const getConnection = async () => {
   if (!pool) {
