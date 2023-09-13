52de68a83086ca2467293b93f86a88808e709337
Sam <ibtissamommari@gmail.com>
2023-09-13 23:58:02 +0200

    Create users/register endpoint

--- back-end/users.js
+++ back-end/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('./db/db');
+const Joi = require('joi');
 
 router.get('/', async (req, res) => {
   try {
@@ -12,6 +13,30 @@ router.get('/', async (req, res) => {
     console.error('Error obteniendo o consultando la conexión a la base de datos:', error);
     return res.status(500).send('Error obteniendo o consultando la conexión a la base de datos');
   }
+});
+
+const registerSchema = Joi.object({
+  username: Joi.string().alphanum().min(3).max(30).required(),
+  password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,20}$')).required(),
+  // email: Joi.string().email().required(),
+});
+
+router.post('/register', async function (req, res) {
+  const { value, error } = registerSchema.validate(req.body);
+  if (error) {
+    return res.status(400).send(error);
+  }
+  const { username, password } = value;
+  try {
+    const connection = await pool
+      .query('INSERT INTO users (name_user, password_user) VALUES (?, ?)', [
+        username,
+        password,
+      ]);
+  } catch (ex) {
+    return res.status(500).send(ex);
+  }
+  return res.json({ username });
 });
 
 module.exports = router;
