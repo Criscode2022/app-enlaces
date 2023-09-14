2d9adabac43ae2f1255563e7c90ec15abf493598
Ignacio S.P <137695280+Nachosprada@users.noreply.github.com>
2023-09-14 19:41:25 +0200

    Add files via upload

---
+++ db/db.js
@@ -0,0 +1,49 @@
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+// Creamos el objeto de configuración para conectarnos a la BBDD
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
+    database: 'enlaces',
+    timezone: 'local'
+};
+
+async function connectToDBPool () {
+    let pool;
+
+    try {
+
+        if (!pool) {
+            pool = await mysql.createPool(dbConfig);
+        }
+
+        return await pool.getConnection();
+
+    } catch (error) {
+        throw new Error(error.message);
+    }
+
+}
+
+async function sendQuery (query, values) {
+  let connection;
+  try {
+
+      connection = await connectToDBPool();
+      const [results] = await connection.query(query, values);
+      return results;
+
+  } catch (error) {
+      throw new Error(error.message);
+  } finally {
+      if (connection) {
+          connection.release();
+      }
+  }
+}
+
+module.exports = sendQuery;
+
---
+++ index.js
@@ -0,0 +1,43 @@
+"use strict";
+// Importaciones usando CommonJS
+const express = require("express");
+const app = express();
+const { createPool } = require('./db/db.js'); // Importa createPool
+const  getLinksController  = require('./controllers/getLinksController.js')
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("El servidor está activo!");
+}); // Ruta para mostrar un mensaje en la raíz del servidor
+
+app.get('/users', async (req, res) => {
+  try {
+    const pool = await createPool();
+    const connection = await pool.getConnection();
+
+    // Consulta a la base de datos
+    const [rows, fields] = await connection.query('SELECT * FROM users');
+    connection.release(); // Devolver la conexión al pool
+
+    console.log('Resultados de la consulta:', rows);
+    res.json(rows);
+  } catch (error) {
+    console.error('Error obteniendo o consultando la conexión a la base de datos:', error);
+    return res.status(500).send('Error obteniendo o consultando la conexión a la base de datos');
+  }
+});
+
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.status(error.httpStatus || 500).send({
+    status: "error",
+    message: error.message,
+  });
+});
+
+//Endpoint para mostrar todos los enlaces publicados:
+app.get('/links', getLinksController);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
