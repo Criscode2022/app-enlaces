ba6f7a1aa70f58097e4336a762f6d9d4d3049b36
Cristian Damil <cristiancode2023@gmail.com>
2023-09-17 13:43:39 +0200

    fix lost parts of index.js

--- back-end/index.js
+++ back-end/index.js
@@ -4,19 +4,25 @@ require('dotenv').config();
 
 // Importaciones usando CommonJS
 const express = require('express');
-const getLinksController = require('./controllers/getLinksController');
-
-//Creamos servidor y aplicamos primeros middlewares:
+const session = require('express-session');
 const app = express();
-app.use(express.json());
-
 
 app.get('/', (req, res) => {
   res.send('This server is now live!');
 }); // Ruta para mostrar un mensaje en la raíz del servidor
 
-//Endpoint para acceder a todos los enlances publicados:
-app.use('/links', getLinksController);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(
+  session({
+    secret: process.env.SESSION_COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use('/users', require('./users'));
 
 //Middleware de errores, devuelve una respuesta de error adecuada y maneja la situación de manera controlada.
 app.use((error, req, res, next) => {
