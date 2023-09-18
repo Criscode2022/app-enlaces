ae4eed5ff78bfb729c9686c6deea9623fbc3ef31
RaquelCP13 <r.caspuyol@gmail.com>
2023-09-18 18:53:43 +0200

    Todas las actualizaciones de usuario

--- back-end/index.js
+++ back-end/index.js
@@ -1,21 +1,21 @@
 "use strict";
 
-require('dotenv').config();
+require("dotenv").config();
 
 // Importaciones usando CommonJS
-const express = require('express');
-const session = require('express-session');
+const express = require("express");
+const session = require("express-session");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('This server is now live!');
+app.get("/", (req, res) => {
+  res.send("This server is now live!");
 }); // Ruta para mostrar un mensaje en la raíz del servidor
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Endpoint para acceder a todos los enlances publicados:
-app.use('/posts', require('./controllers/postsController'));
+app.use("/posts", require("./controllers/postsController"));
 
 app.use(
   session({
@@ -25,7 +25,11 @@ app.use(
   })
 );
 
-app.use('/users', require('./controllers/usersController'));
+app.use("/users", require("./controllers/usersController"));
+app.use("/update-biography", require("./controllers/updatesUsers"));
+app.use("/update-avatar", require("./controllers/updatesUsers"));
+app.use("/change-name", require("./controllers/updatesUsers"));
+app.use("/change-password", require("./controllers/updatesUsers"));
 
 //Middleware de errores, devuelve una respuesta de error adecuada y maneja la situación de manera controlada.
 app.use((error, req, res, next) => {
