784a37e5a3537705a3f4ae5236b49d8ea0d559ea
gabrielanioncaamanho <139446733+gabrielanioncaamanho@users.noreply.github.com>
2023-09-21 17:12:13 +0200

    Middleware 404 notfound

--- back-end/controllers/postsController.js
+++ back-end/controllers/postsController.js
@@ -1,28 +1,26 @@
 const pool = require('../db/db');
 
-async function getPostsController (req, res, next) {
-    const connection = await pool.getConnection();
-    try {
-        const [results] = await pool.query(`
+async function getPostsController(req, res, next) {
+  const connection = await pool.getConnection();
+  try {
+    const [results] = await pool.query(`
         SELECT post_url, post_title, post_description, name_user, posts.created_at, post_likes
         FROM posts 
         JOIN users 
         ON posts.id_user = users.id_user      
         `);
 
-        res.send({
-            ok: true,
-            data: results,
-            error: null,
-            message: null
-        });
-
-    } catch (error) {
-        next(error);
-    } finally {
-        if (connection) connection.release();
-    }
-
+    res.send({
+      ok: true,
+      data: results,
+      error: null,
+      message: null,
+    });
+  } catch (error) {
+    next(error);
+  } finally {
+    if (connection) connection.release();
+  }
 }
 
 async function likePostController(req, res) {
