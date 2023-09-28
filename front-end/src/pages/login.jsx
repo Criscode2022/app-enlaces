4a879160159f0713d0f9cd21755b5bcddeb8aaec
Sam <ibtissamommari@gmail.com>
2023-09-28 20:16:45 +0200

    Add login form markup.
    Not working yet.

--- front-end/src/pages/login.jsx
+++ front-end/src/pages/login.jsx
@@ -1,6 +1,17 @@
 const Login = () => {
     return (
+      <>
         <h2>Login</h2>
+        <form>
+          <label>
+            Usuario <input type="text" name="user" />
+          </label>
+          <label>
+            Contraseña <input type="password" name="password" />
+          </label>
+          <button type="submit">Enviar</button>
+        </form>
+      </>
     );
 };
 
