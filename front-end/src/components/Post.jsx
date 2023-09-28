50ddf30b562d44e63bfab73fcc13a47e884f1c78
Cristian Damil <cristiancode2023@gmail.com>
2023-09-28 17:16:53 +0200

    Base para gestionar los likes desde React

--- front-end/src/components/Post.jsx
+++ front-end/src/components/Post.jsx
@@ -1,18 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Badge from '@mui/material/Badge';
 
 const Post = (props) => {
-  const { imageUrl, title, content, url } = props;
+  const { imageUrl, title, content, url, postId } = props;
+  const [likes, setLikes] = useState(0); 
+
+  const incrementLikes = () => {
+    fetch(`/api/posts/${postId}/like`, {
+      method: 'POST',
+      headers: {
+      },
+    })
+      .then((response) => {
+        if (response.status === 201) {
+          return response.json();
+        } else {
+          throw new Error('No se pudo dar like');
+        }
+      })
+      .then((data) => {
+        setLikes(data.likes);
+      })
+      .catch((error) => {
+        console.error('Error al dar like:', error);
+      });
+  };
 
   return (
-<div className="card" style={{ width: '18rem', border: '1px solid grey', borderRadius: '10px', margin: '20px', boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)' }}>
-      <img src={imageUrl} className="card-img-top" alt="Post Image" />
-      <div className="card-body">
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text">{content}</p>
-        <a href={url} target='_blank' className="btn btn-primary">Visitar</a>
-      </div>
-    </div>
+    <Card sx={{ maxWidth: 345, margin: '20px', padding: '50px' }}>
+      <CardMedia component="img" height="140" image={imageUrl} alt="Post Image" />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {content}
+        </Typography>
+        <Button variant="contained" href={url} target="_blank" fullWidth>
+          Visitar
+        </Button>
+        <Badge badgeContent={likes} color="primary">
+          <Button variant="outlined" onClick={incrementLikes}>
+            Like
+          </Button>
+        </Badge>
+      </CardContent>
+    </Card>
   );
-}
+};
 
 export default Post;
