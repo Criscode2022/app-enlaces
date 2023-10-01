c0fb94f85d30d676b5d61737b31443e08b2ba300
Sam <ibtissamommari@gmail.com>
2023-10-01 17:54:52 +0200

    login: Make login form work.
    Store the token in localStorage.

--- front-end/src/pages/login.jsx
+++ front-end/src/pages/login.jsx
@@ -1,11 +1,33 @@
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
+const handleSubmit = async e => {
+  e.preventDefault();
+  const data = new FormData(e.target);
+  const result = {};
+  data.forEach((k, v) => result[v] = k);
+  console.log(result);
+  const response = await fetch("http://localhost:3000/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(result),
+  });
+  const json = await response.json();
+  if (json.error) {
+    alert(json.error);
+    return;
+  }
+  window.localStorage.setItem("login-token", json.token);
+  alert("Successfully logged in");
+};
+
 const Login = () => {
     return (
       <>
         <h2>Login</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <TextField required={true} label="Usuario" name="username" />
           <TextField required={true} label="Contraseña" type="password" name="password" />
           <Button type="submit">Enviar</Button>
