d1a64fa1b9c2e8b68bb84d0cd6ef43e47a4a3417
RaquelCP13 <r.caspuyol@gmail.com>
2023-10-02 19:10:26 +0200

    Formulario de enlace con boton en header

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,34 +1,37 @@
-import { useState } from 'react';
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Header from './layout/Header';
-import Tabs from './layout/Tabs';
-import About from './pages/about';
-import Login from './pages/login';
-import Register from './pages/register';
-import Home from './pages/home';
-import Error404 from './pages/error404';
-import Avatar from './pages/avatar';
-import Feed from './pages/Feed';
+// eslint-disable-next-line no-unused-vars
+import { useState } from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Header from "./layout/Header";
+// eslint-disable-next-line no-unused-vars
+import Tabs from "./layout/Tabs";
+import About from "./pages/about";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import Home from "./pages/home";
+import Error404 from "./pages/error404";
+import Avatar from "./pages/avatar";
+import Feed from "./pages/Feed";
+import ShareLink from "./pages/ShareLink";
 
 function App() {
-
   return (
     <>
       <Header />
       <main>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/avatar' element={<Avatar />} />
-          <Route path='/*' element={<Error404 />} />
-          <Route path='/feed' element={<Feed />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/avatar" element={<Avatar />} />
+          <Route path="/linkform" element={<ShareLink />} />
+          <Route path="/*" element={<Error404 />} />
+          <Route path="/feed" element={<Feed />} />
         </Routes>
       </main>
     </>
-  )
+  );
 }
 
 export default App;
---
+++ front-end/src/components/LinkForm.jsx
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+function LinkForm() {
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
+  const [description, setDescription] = useState("");
+  const [username, setUsername] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  // Función para manejar el envío del formulario
+  function handleSubmit(event) {
+    event.preventDefault();
+    const data = { title, url, description, username };
+
+    fetch("/api/post", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("No se pudo crear el enlace");
+        }
+        return response.json();
+      })
+      .then(() => {
+        // Manejar la respuesta del servidor aquí
+        setSuccessMessage("Enlace creado exitosamente");
+        setErrorMessage(""); // Limpiar cualquier mensaje de error previo
+      })
+      .catch((error) => {
+        // Manejar errores de la solicitud aquí
+        setSuccessMessage(""); // Limpiar cualquier mensaje de éxito previo
+        setErrorMessage(error.message || "Hubo un error al crear el enlace");
+      });
+  }
+
+  // Funciones para manejar cambios en los campos del formulario
+  function handleTitleChange(event) {
+    setTitle(event.target.value);
+  }
+
+  function handleUrlChange(event) {
+    setUrl(event.target.value);
+  }
+
+  function handleDescriptionChange(event) {
+    setDescription(event.target.value);
+  }
+
+  function handleUsernameChange(event) {
+    setUsername(event.target.value);
+  }
+
+  return (
+    <div>
+      {successMessage && <p className="success-message">{successMessage}</p>}
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
+      <form onSubmit={handleSubmit}>
+        <TextField
+          required
+          label="Título"
+          value={title}
+          onChange={handleTitleChange}
+        />
+        <TextField
+          required
+          label="URL"
+          value={url}
+          onChange={handleUrlChange}
+        />
+        <TextField
+          required
+          label="Descripción"
+          value={description}
+          onChange={handleDescriptionChange}
+        />
+        <TextField
+          required
+          label="Nombre de Usuario"
+          value={username}
+          onChange={handleUsernameChange}
+        />
+        <Button type="submit">Crear Enlace</Button>
+      </form>
+    </div>
+  );
+}
+
+export default LinkForm;
