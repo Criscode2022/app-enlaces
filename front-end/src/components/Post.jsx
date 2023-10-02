348535d1556d88e8a5b6f3bc69ed0b0282b89e47
Cristian Damil <cristiancode2023@gmail.com>
2023-10-03 00:01:40 +0200

    follow functionality added without check

--- front-end/src/components/Post.jsx
+++ front-end/src/components/Post.jsx
@@ -10,14 +10,18 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import jwt_decode from 'jwt-decode';
 
 const Post = (props) => {
-  const { imageUrl, title, content, url, postId, userId } = props;
+  const { imageUrl, title, content, url, postId, userId, userName } = props;
   const [likes, setLikes] = useState(0); // Estado para el contador de likes
   const [userLiked, setUserLiked] = useState(false); // Estado para saber si el usuario dio like
   const [authenticatedUserId, setAuthenticatedUserId] = useState(null); // ID del usuario autenticado
+  const [isFollowing, setIsFollowing] = useState(false); // Estado para saber si el usuario está siguiendo a la persona
 
   useEffect(() => {
     // Obtener el token del localStorage
     const token = localStorage.getItem('login-token');
+
+    // Llama a la función checkFollowing para obtener el estado inicial de seguimiento
+    checkFollowing(userId, setIsFollowing, token);
 
     // Decodificar el token para obtener el ID del usuario autenticado
     try {
@@ -48,7 +52,7 @@ const Post = (props) => {
 
         // Actualizar el estado de userLiked
         setUserLiked(userLikedPost);
-        console.log('Likes API Data:', data);
+        console.log('Likes  API Data:', data);
         console.log('User Liked Post:', userLikedPost);
       })
       .catch((error) => {
@@ -75,7 +79,7 @@ const Post = (props) => {
       .catch((error) => {
         console.error('Error al obtener el contador de likes:', error);
       });
-  }, [postId, authenticatedUserId]);
+  }, [postId, authenticatedUserId, userId]);
 
   const toggleLike = () => {
     // Realizar una solicitud a la API para dar o quitar like en función de userLiked
@@ -112,31 +116,99 @@ const Post = (props) => {
       });
   };
 
+  // Consulta para verificar si el usuario está siguiendo al usuario deseado
+  const checkFollowing = () => {
+    // Realizar una solicitud GET a la API para verificar si el usuario sigue a la persona
+    const endpoint = `http://localhost:3000/users/isFollowing/${userId}`;
+  
+    // Obtener el token del localStorage
+    const token = localStorage.getItem('login-token');
+  
+    fetch(endpoint, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${token}`,
+      },
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          throw new Error('Error al verificar el seguimiento del usuario');
+        }
+      })
+      .then((data) => {
+        // Actualiza el estado isFollowing en función de la respuesta de la API
+        setIsFollowing(data.isFollowing);
+        console.log('Is Following:', data.isFollowing);
+      })
+      .catch((error) => {
+        console.error('Error al verificar el seguimiento del usuario:', error);
+      });
+  };
+
+  const handleFollow = () => {
+    // Realizar una solicitud a la API para seguir o dejar de seguir al usuario
+    const endpoint = `http://localhost:3000/users/follow/${userId}`;
+
+    // Obtener el token del localStorage
+    const token = localStorage.getItem('login-token');
+
+    fetch(endpoint, {
+      method: isFollowing ? 'DELETE' : 'PUT', // Cambia el método en función de si el usuario ya está siguiendo
+      headers: {
+        'Authorization': `Bearer ${token}`,
+      },
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          console.log(isFollowing ? 'Dejaste de seguir al usuario.' : 'Usuario seguido con éxito.');
+          // Actualiza el estado de isFollowing en función del resultado
+          setIsFollowing(!isFollowing);
+        } else if (response.status === 400) {
+          console.log('Ya sigues a este usuario.');
+        } else {
+          throw new Error('No se pudo seguir al usuario');
+        }
+      })
+      .catch((error) => {
+        console.error('Error al seguir al usuario:', error);
+      });
+  };
+
   console.log('Post ID:', postId);
   console.log('Authenticated User ID:', authenticatedUserId);
 
   return (
     <Card sx={{ maxWidth: 345, margin: '20px', padding: '50px', boxShadow:'0 0 10px black' }}>
-      <CardMedia component="img" height="140" image={imageUrl} alt="Post Image" />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {title}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {content}
-        </Typography>
-        <Button variant="contained" href={url} target="_blank" fullWidth>
-          Visitar
-        </Button>
-        <Badge badgeContent={likes} color="primary">
-          <Button variant="outlined" onClick={toggleLike} style={{ color: 'red' }}>
-            {userLiked ? <FavoriteIcon /> : <FavoriteBorderOutlinedIcon />}
+      <div style={{ display: 'flex', alignItems: 'center', padding: '10px' }}>
+        <Typography variant="h6" component="div" style={{ flexGrow: 1, border: '1px solid black', padding: '5px', width: '100%', borderRadius:'5px', display: 'flex', justifyContent: 'space-between' }}>
+          {userName}
+          <Button variant="contained" style={{ backgroundColor: isFollowing ? '#f00' : '#007bff', color: 'white' }} onClick={handleFollow}>
+            {isFollowing ? 'Dejar de seguir' : 'Seguir'}
           </Button>
-        </Badge>
-      </CardContent>
-    </Card>
+        </Typography>
+      </div>
+  <CardMedia component="img" height="140" image={imageUrl} alt="Post Image" />
+  <CardContent>
+    <Typography gutterBottom variant="h5" component="div">
+      {title}
+    </Typography>
+    <Typography variant="body2" color="text.secondary">
+      {content}
+    </Typography>
+    <Button variant="contained" href={url} target="_blank" fullWidth>
+      Visitar
+    </Button>
+    <Badge badgeContent={likes} color="primary">
+      <Button variant="outlined" onClick={toggleLike} style={{ color: 'red' }}>
+        {userLiked ? <FavoriteIcon /> : <FavoriteBorderOutlinedIcon />}
+      </Button>
+    </Badge>
+  </CardContent>
+</Card>
+
   );
 };
 
 export default Post;
-
