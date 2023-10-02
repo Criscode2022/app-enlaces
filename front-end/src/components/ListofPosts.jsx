e6dc88bc6d3eda862cbabb7fc2ce98a278480b4f
Cristian Damil <cristiancode2023@gmail.com>
2023-10-03 00:01:40 +0200

    follow functionality added without check

--- front-end/src/components/ListofPosts.jsx
+++ front-end/src/components/ListofPosts.jsx
@@ -24,7 +24,7 @@ const ListofPosts = () => {
     <div>
       <h2>Enlaces</h2>
       {postsData.map((post) => (
-        <Post key={post.id_post} title={post.post_title} content={post.post_description} url={post.post_url} imageUrl={post.post_url} postId={post.id_post} userId={post.id_user} />
+        <Post key={post.id_post} title={post.post_title} content={post.post_description} url={post.post_url} imageUrl={post.post_url} postId={post.id_post} userId={post.id_user} userName={post.name_user} />
       ))}
     </div>
   );
