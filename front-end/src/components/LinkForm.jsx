446a75100d9566f29ff953205e0543052b1d8c36
RaquelCP13 <r.caspuyol@gmail.com>
2023-10-03 19:39:22 +0200

    Últimos cambios con id_user y pasado por token

--- front-end/src/components/LinkForm.jsx
+++ front-end/src/components/LinkForm.jsx
@@ -2,42 +2,50 @@ import { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
-function LinkForm() {
+function LinkForm({ id_user }) {
   const [title, setTitle] = useState("");
   const [url, setUrl] = useState("");
   const [description, setDescription] = useState("");
-  const [username, setUsername] = useState("");
   const [successMessage, setSuccessMessage] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
 
   // Función para manejar el envío del formulario
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
-    const data = { title, url, description, username };
+    const data = { title, url, description, id_user }; // Incluir el id_user en los datos
 
-    fetch("/api/post", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("No se pudo crear el enlace");
-        }
-        return response.json();
-      })
-      .then(() => {
-        // Manejar la respuesta del servidor aquí
-        setSuccessMessage("Enlace creado exitosamente");
-        setErrorMessage(""); // Limpiar cualquier mensaje de error previo
-      })
-      .catch((error) => {
-        // Manejar errores de la solicitud aquí
-        setSuccessMessage(""); // Limpiar cualquier mensaje de éxito previo
-        setErrorMessage(error.message || "Hubo un error al crear el enlace");
+    // Obtener el token del almacenamiento local
+    const token = window.localStorage.getItem("login-token");
+
+    console.log("Datos del formulario:", data); // Agregado para verificar los datos del formulario
+    console.log("Token de autenticación:", token); // Agregado para verificar el token
+
+    try {
+      const response = await fetch(`http://localhost:3000/users/posts`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          // Adjuntar el token en el encabezado de la solicitud
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(data),
       });
+
+      if (!response.ok) {
+        throw new Error("No se pudo crear el enlace");
+      }
+
+      const responseData = await response.json();
+      // Manejar la respuesta del servidor aquí, si es necesario.
+
+      setSuccessMessage("Enlace creado exitosamente");
+      setErrorMessage(""); // Limpiar cualquier mensaje de error previo
+    } catch (error) {
+      // Manejar errores de la solicitud aquí
+      console.error("Error al crear el enlace:", error);
+      setSuccessMessage(""); // Limpiar cualquier mensaje de éxito previo
+      setErrorMessage(error.message || "Hubo un error al crear el enlace");
+    }
   }
 
   // Funciones para manejar cambios en los campos del formulario
@@ -53,14 +61,8 @@ function LinkForm() {
     setDescription(event.target.value);
   }
 
-  function handleUsernameChange(event) {
-    setUsername(event.target.value);
-  }
-
   return (
     <div>
-      {successMessage && <p className="success-message">{successMessage}</p>}
-      {errorMessage && <p className="error-message">{errorMessage}</p>}
       <form onSubmit={handleSubmit}>
         <TextField
           required
@@ -80,14 +82,10 @@ function LinkForm() {
           value={description}
           onChange={handleDescriptionChange}
         />
-        <TextField
-          required
-          label="Nombre de Usuario"
-          value={username}
-          onChange={handleUsernameChange}
-        />
         <Button type="submit">Crear Enlace</Button>
       </form>
+      {successMessage && <p className="success-message">{successMessage}</p>}
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
     </div>
   );
 }
--- front-end/src/pages/home.jsx
+++ front-end/src/pages/home.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import Tabs from '../layout/Tabs';
+import React from "react";
+import Tabs from "../layout/Tabs";
 
 const Home = () => {
-    return (<>
-        <Tabs></Tabs>
-        <h2>Home</h2>
-        </>
-    );
+  return (
+    <>
+      <Tabs></Tabs>
+      <h2>Home</h2>
+    </>
+  );
 };
 
 export default Home;
