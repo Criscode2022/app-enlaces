38a80298d69fef032ecc5ee2082658e02138d058
Cristian Damil <cristiancode2023@gmail.com>
2023-10-06 17:05:13 +0200

    Uploaded AuthContext

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,5 +1,4 @@
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate, Outlet } from "react-router-dom";
 import Header from "./layout/Header";
 import AboutPage from "./pages/AboutPage";
 import LoginPage from "./pages/LoginPage";
@@ -9,23 +8,27 @@ import Error404Page from "./pages/Error404Page";
 import AvatarPage from "./pages/AvatarPage";
 import FeedPage from "./pages/FeedPage";
 import NewPostPage from "./pages/NewPostPage";
+import { useAuth } from './tuArchivoDeAutenticacion'; // Importa la función de autenticación adecuada
 
 function App() {
+  const isAuthenticated = useAuth(); // Supongamos que esta función verifica la autenticación
+
   return (
     <>
       <Header />
       <main>
         <Routes>
-          <Route path="/" element={<LoginPage />} />
+          <Route path="/" element={isAuthenticated ? <Navigate to="/feed" /> : <LoginPage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/logout" element={<LogoutPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/avatar" element={<AvatarPage />} />
           <Route path="/newPost" element={<NewPostPage />} />
-          <Route path="/*" element={<Error404Page />} />
           <Route path="/feed" element={<FeedPage />} />
+          <Route path="/*" element={<Error404Page />} />
         </Routes>
+        <Outlet /> {/* Muestra las rutas secundarias dentro de esta Outlet */}
       </main>
     </>
   );
---
+++ front-end/src/context/AuthContext.jsx
@@ -0,0 +1,24 @@
+import { createContext, useEffect } from "react";
+
+// Creamos el contexto de autenticación.
+export const AuthContext = createContext(null);
+
+// Creamos el componente provider.
+export const AuthProvider = ({ children }) => {
+    const [authUser, setAuthUser] = useState(null);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        // Función que retorna los datos del usuario.
+        const fetchUser = async () => {
+            try {
+                setLoading(true);
+                
+            } catch (err) {
+                
+            } finally {
+                setLoading(false);
+            }
+        }
+    }, []);
