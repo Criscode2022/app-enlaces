d21d729ead68ddea3169a27e7d9a7ab1a8aadd83
Sam <ibtissamommari@gmail.com>
2023-10-08 14:44:42 +0200

    RegisterPage: Navigate to feed if logged in.

--- front-end/src/pages/RegisterPage.jsx
+++ front-end/src/pages/RegisterPage.jsx
@@ -1,6 +1,13 @@
+import { useContext } from 'react';
 import RegisterForm from '../components/Forms/RegisterForm'
+import { AuthContext } from '../context/AuthContext';
+import { Navigate } from 'react-router-dom';
 
 const RegisterPage = () => {
+  const auth = useContext(AuthContext);
+  if (auth.token) {
+    return <Navigate to="/feed"/>
+  }
   return (
     <>
       <h2>Registrarse</h2>
