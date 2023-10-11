c95f570f7133389d690201a20fd77f02f2fe0b8a
Cristian Damil <cristiancode2023@gmail.com>
2023-10-11 10:16:25 +0200

    Filters of following users working

--- front-end/src/components/ListofPostsFollowing.jsx
+++ front-end/src/components/ListofPostsFollowing.jsx
@@ -4,48 +4,57 @@ import { AuthContext } from '../context/AuthContext';
 
 const ListofPostsFollowing = () => {
   const [postsData, setPostsData] = useState([]);
-  const [badgeCounts, setBadgeCounts] = useState({}); // Track badge counts by postId
-  const { token } = useContext(AuthContext); // Obtener el token del contexto de autenticación
+  const [badgeCounts, setBadgeCounts] = useState({});
+  const { token } = useContext(AuthContext);
 
   useEffect(() => {
-    // Realizar la solicitud HTTP para obtener los datos de localhost:3000/posts
-    fetch('http://localhost:3000/posts', {
+    // Fetch the list of users you are following
+    fetch('http://localhost:3000/users/following', {
       headers: {
         Authorization: `Bearer ${token}`,
       },
     })
-      .then((response) => response.json())
-      .then((data) => {
-        // Extraer la propiedad 'data' del objeto JSON
-        const postDataArray = data.data;
+      .then((followResponse) => followResponse.json())
+      .then((followData) => {
+        console.log('Follow Data:', followData);
 
-        // Initialize badge counts
-        const initialBadgeCounts = {};
-        postDataArray.forEach((post) => {
-          initialBadgeCounts[post.id_post] = post.like_count;
-        });
-        setBadgeCounts(initialBadgeCounts);
+        // Extract user IDs from the followData
+        const usersFollowed = followData;
+        console.log('Users Followed:', usersFollowed);
 
-        // Filtrar los posts para mostrar solo los de usuarios seguidos
-        fetch(`http://localhost:3000/users/following`, {
+        // Fetch the list of posts
+        fetch('http://localhost:3000/posts', {
           headers: {
             Authorization: `Bearer ${token}`,
           },
         })
-          .then((followResponse) => followResponse.json())
-          .then((followData) => {
-            const usersFollowed = followData.users.map((user) => user.id_user);
+          .then((response) => response.json())
+          .then((data) => {
+            console.log('Posts Data:', data);
+
+            const postDataArray = data.data;
+
+            // Initialize badge counts
+            const initialBadgeCounts = {};
+            postDataArray.forEach((post) => {
+              initialBadgeCounts[post.id_post] = post.like_count;
+            });
+            setBadgeCounts(initialBadgeCounts);
+
+            // Filter the posts based on the following users
             const filteredPosts = postDataArray.filter((post) =>
               usersFollowed.includes(post.id_user)
             );
+            console.log('Filtered Posts:', filteredPosts);
+
             setPostsData(filteredPosts);
           })
           .catch((error) => {
-            console.error('Error al obtener la lista de usuarios seguidos:', error);
+            console.error('Error al obtener los datos de los posts:', error);
           });
       })
       .catch((error) => {
-        console.error('Error al obtener los datos de los posts:', error);
+        console.error('Error al obtener la lista de usuarios seguidos:', error);
       });
   }, [token]);
 
@@ -61,7 +70,7 @@ const ListofPostsFollowing = () => {
     <>
       <h2>Enlaces</h2>
 
-      <div id='postsContainer'>
+      <div id="postsContainer">
         {postsData.map((post) => (
           <Post
             key={post.id_post}
@@ -73,12 +82,12 @@ const ListofPostsFollowing = () => {
             userId={post.id_user}
             userName={post.name_user}
             likes={post.like_count}
-            updateBadgeCount={updateBadgeCount} // Pass the callback function
+            updateBadgeCount={updateBadgeCount}
           />
         ))}
       </div>
     </>
   );
-}
+};
 
 export default ListofPostsFollowing;
