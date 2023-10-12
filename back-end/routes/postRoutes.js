2585b125d5bf2e3c321db402e094adec489e5858
Cristian Damil <cristiancode2023@gmail.com>
2023-10-12 18:28:04 +0200

    fix authenticateToken missing on postRoutes

--- back-end/routes/postRoutes.js
+++ back-end/routes/postRoutes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const deletePostController = require('../controllers/posts/deletePostController');
 const routes = express.Router();
 
+// Importamos las funciones controladoras intermedias.
+const authenticateToken = require('../middlewares/authenticateToken');
+
 // Importamos las funciones controladoras finales.
 
 const {
@@ -14,12 +17,11 @@ const {
 
 // Aquí irían el resto de enpoints de posts.
 
-routes.get('/posts', getPostsController);
-routes.post('/posts/like/:postId', likePostController);
-routes.get('/posts/likes', getAllLikesController);
-routes.delete('/posts/unlike/:postId', unlikePostController);
-routes.post('/posts/newPost', createPost);
-routes.delete('/posts/:postId', deletePostController);
+routes.get('/posts', authenticateToken, getPostsController);
+routes.post('/posts/like/:postId', authenticateToken, likePostController);
+routes.get('/posts/likes', authenticateToken, getAllLikesController);
+routes.delete('/posts/unlike/:postId', authenticateToken, unlikePostController);
+routes.post('/posts/newPost', authenticateToken, createPost);
+routes.delete('/posts/:postId', authenticateToken, deletePostController);
 
 module.exports = routes;
-
