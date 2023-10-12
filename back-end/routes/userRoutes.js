385316dd7f80d025c7df9bef26e1765a91f25a89
Cristian Damil <cristiancode2023@gmail.com>
2023-10-12 20:07:00 +0200

    fix duplicated endpoint

--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -12,12 +12,8 @@ const {
     followUserController,
     loginUserController,
     unfollowUserController,
-    getUserByIdController
+    getUserByIdController,
 } = require('../controllers/users');
-
-//Check follow de usuario.
-
-routes.get('/users/checkfollow/:userFollow', checkFollowController);
 
 //Obtener información de un usuario por parámetro id:
 routes.get('/users/:userId', authenticateToken, getUserByIdController);
