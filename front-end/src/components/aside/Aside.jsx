e33bb66aceb1147a75d7b50cfeb7d97ef35ea42a
Ignacio S.P <137695280+Nachosprada@users.noreply.github.com>
2023-10-14 20:58:32 +0200

    Created Aside component for login and register

---
+++ front-end/src/components/aside/Aside.jsx
@@ -0,0 +1,14 @@
+import "./Aside.css";
+
+const Aside = () => {
+    return (
+    <aside className="aside-text">
+        <h3>Sobre nosotros</h3>
+        <hr />
+        <p>Somos un joven equipo de desarrollo web con ganas de ofrecer al usuario productos interactivos de calidad. Nuestra aplicación ofrece al usuario la posibilidad de compartir sus enlaces de interés y seguir a otros publicadores de su elección.     
+        </p>
+    </aside>
+    );
+};
+
+export default Aside;
