ca7c4679153b57bba5302e83a0033a44ebaaaeb7
Sam <ibtissamommari@gmail.com>
2023-10-15 22:57:59 +0200

    back-end: Make avatar and biography really optional.
    Otherwise empty updates don't work.

--- back-end/schemas/users/updateUserSchema.js
+++ back-end/schemas/users/updateUserSchema.js
@@ -2,8 +2,8 @@
 
 const Joi = require('joi');
 const updateUserSchema = Joi.object({
-    biography: Joi.string().max(200),
-    avatar: Joi.string().uri(),
+    biography: Joi.string().max(200).allow(null, '').optional(),
+    avatar: Joi.string().uri().allow(null, '').optional(),
     username: Joi.string().min(3).max(30),
     newPassword: Joi.string()
         .pattern(new RegExp('^[a-zA-Z0-9]{3,20}$'))
