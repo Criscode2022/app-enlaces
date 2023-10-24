f5a6d4a0e85c906fdebf03e0f537918cae95e408
Cristian Damil <cristiancode2023@gmail.com>
2023-10-24 17:44:27 +0200

    Definitive updateForm and errors on Material UI forms

--- front-end/src/components/Forms/UpdateForm.jsx
+++ front-end/src/components/Forms/UpdateForm.jsx
@@ -1,12 +1,9 @@
-import "../../App.css";
-import { useContext, useState } from "react";
+import React, { useContext, useState } from "react";
 import TextField from "@mui/material/TextField";
-import { AuthContext } from "../../context/AuthContext";
 import Button from "@mui/material/Button";
+import { AuthContext } from "../../context/AuthContext";
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { useNavigate } from "react-router-dom";
-
 
 function UpdateForm() {
   const [formData, setFormData] = useState({
@@ -15,28 +12,80 @@ function UpdateForm() {
     avatar: "",
     newPassword: "",
   });
+
+  const [errors, setErrors] = useState({
+    username: "",
+    biography: "",
+    avatar: "",
+    newPassword: "",
+  });
+
   const auth = useContext(AuthContext);
-  const navigate = useNavigate();
   const { token } = useContext(AuthContext);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
+    setErrors({ ...errors, [name]: "" }); // Limpiar el mensaje de error del campo actual
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      await auth.update(formData);
-      toast.success("Perfil actualizado correctamente", {
-        position: toast.POSITION.TOP_CENTER
-      });
+      // Filtrar los campos que tienen valores
+      const filteredFormData = Object.keys(formData).reduce((acc, key) => {
+        if (formData[key]) {
+          acc[key] = formData[key];
+        }
+        return acc;
+      }, {});
+
+      const url = "http://localhost:3000/users/update";
+      const requestOptions = {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(filteredFormData),
+      };
+      const response = await fetch(url, requestOptions);
+      const jsonResponse = await response.json();
+
+      if (response.status === 400) {
+        // Si la respuesta tiene un estado 400 (Bad Request), maneja los errores
+        setErrors({
+          username: jsonResponse.fields.includes("username")
+            ? jsonResponse.error
+            : "",
+          biography: jsonResponse.fields.includes("biography")
+            ? jsonResponse.error
+            : "",
+          avatar: jsonResponse.fields.includes("avatar")
+            ? jsonResponse.error
+            : "",
+          newPassword: jsonResponse.fields.includes("newPassword")
+            ? jsonResponse.error
+            : "",
+        });
+      } else if (!response.ok) {
+        throw new Error(jsonResponse.message);
+      } else {
+        auth.update(formData); // Actualiza el token en el AuthContext
+        toast.success("Perfil actualizado correctamente", {
+          position: toast.POSITION.TOP_CENTER
+        })
+        // Limpia los mensajes de error de todos los campos en caso de éxito
+        setErrors({
+          username: "",
+          biography: "",
+          avatar: "",
+          newPassword: "",
+        });
+      }
     } catch (error) {
       console.error("Error updating profile:", error);
-      toast.error(error.message || "Error updating profile", {
-        position: toast.POSITION.TOP_CENTER
-      })
     }
   };
 
@@ -49,6 +98,8 @@ function UpdateForm() {
         id="username"
         name="username"
         value={formData.username}
+        helperText={errors.username}
+        error={Boolean(errors.username)}
       />
       <TextField
         type="text"
@@ -57,6 +108,8 @@ function UpdateForm() {
         id="biography"
         name="biography"
         value={formData.biography}
+        helperText={errors.biography}
+        error={Boolean(errors.biography)}
       />
       <TextField
         type="text"
@@ -65,6 +118,8 @@ function UpdateForm() {
         id="avatar"
         name="avatar"
         value={formData.avatar}
+        helperText={errors.avatar}
+        error={Boolean(errors.avatar)}
       />
       <TextField
         type="password"
@@ -73,8 +128,12 @@ function UpdateForm() {
         id="newPassword"
         name="newPassword"
         value={formData.newPassword}
+        helperText={errors.newPassword}
+        error={Boolean(errors.newPassword)}
       />
-      <Button type="submit" variant="contained">Actualizar perfil</Button>
+      <Button type="submit" variant="contained">
+        Actualizar perfil
+      </Button>
     </form>
   );
 }
