056177cc9925629b3bae16b1860b840804ef0ca2
Cristian Damil <cristiancode2023@gmail.com>
2023-10-24 11:19:26 +0200

    Errors on Material UI Form on RegisterForm

--- front-end/src/components/Forms/RegisterForm.jsx
+++ front-end/src/components/Forms/RegisterForm.jsx
@@ -1,42 +1,55 @@
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { AuthContext } from '../../context/AuthContext';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { toast } from 'react-toastify';
-import { Navigate } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
 
-async function handleSubmit(e, auth) {
-  e.preventDefault();
-  const data = new FormData(e.target);
-  const result = {};
-  data.forEach((k, v) => (result[v] = k));
 
-  try {
-    await auth.register(result.username, result.password);
-    toast.success("Usuario registrado correctamente", {
-      position: toast.POSITION.TOP_CENTER
-    })
-  } catch (error) {
-    toast.error(error.message || "Error registrando usuario", {
-      position: toast.POSITION.TOP_CENTER
-    })
-  }
-}
+
+
 
 
 
 const RegisterForm = () => {
   const auth = useContext(AuthContext);
-  if (auth.token) {
-    return <Navigate to="/feed" />;
+
+  const [errors, setErrors] = useState({
+    username: '',
+    password: '',
+  });
+
+  async function handleSubmit(e, auth) {
+
+    e.preventDefault();
+    const data = new FormData(e.target);
+    const result = {};
+    data.forEach((k, v) => (result[v] = k));
+
+    try {
+      await auth.register(result.username, result.password);
+      toast.success("Usuario registrado correctamente", {
+        position: toast.POSITION.TOP_CENTER
+      })
+    } catch (error) {
+      setErrors({
+        ...errors,
+        username: " ",
+        password: error.message,
+      });
+    }
   }
+
+
   return (
     <div className='flex'>
       <h2>Registrarse</h2>
       <form onSubmit={(e) => handleSubmit(e, auth)}>
-        <TextField required={true} label="Usuario" name="username" />
-        <TextField required={true} label="Contraseña" type="password" name="password" />
+        <TextField required={true} error={Boolean(errors.username)}
+          label="Usuario" name="username" />
+        <TextField required={true} label="Contraseña" helperText={errors.password}
+          error={Boolean(errors.password)}
+          type="password" name="password" />
         <Button disabled={auth.loading} variant="contained" type="submit">
           Enviar
         </Button>
